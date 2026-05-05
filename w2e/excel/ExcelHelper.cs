94f87c865b37e8c07999188814c8ae48750a396a
tanno-seiichi <tantans.yhc@gmail.com>
2026-05-06 01:49:36 +0900

    コメント追加

--- w2e/excel/ExcelHelper.cs
+++ w2e/excel/ExcelHelper.cs
@@ -196,6 +196,7 @@ namespace w2e.excel
                     cell.CellValue = new CellValue( data.text ?? "" );
                 }
 
+                /* セルに枠線を設定 */
                 BorderHelper.ApplyBorder( a_wbPart, cell, data.topBorder, data.bottomBorder, data.leftBorder, data.rightBorder, a_cache );
 
                 row.Append( cell );
