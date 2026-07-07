bf344a29d60005c321efc8e9c6a9bd9f2b8a4112
tanno-seiichi <tantans.yhc@gmail.com>
2026-07-08 00:26:12 +0900

    v1.5

--- w2e/Properties/AssemblyInfo.cs
+++ w2e/Properties/AssemblyInfo.cs
@@ -48,5 +48,5 @@ using System.Windows;
 //      ビルド番号
 //      リビジョン
 //
-[assembly: AssemblyVersion( "1.4.0.0" )]
-[assembly: AssemblyFileVersion( "1.4.0.0" )]
+[assembly: AssemblyVersion( "1.5.0.0" )]
+[assembly: AssemblyFileVersion( "1.5.0.0" )]
