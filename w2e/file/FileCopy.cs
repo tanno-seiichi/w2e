5af4767c16535a563b58ef7d9d1c2e85cdc350fc
tanno-seiichi <tantans.yhc@gmail.com>
2026-08-08 18:21:19 +0900

    Wordファイルを開く時にOSのTempフォルダにコピーしてから開くようにした

--- w2e/file/FileCopy.cs
+++ w2e/file/FileCopy.cs
@@ -14,7 +14,10 @@ namespace w2e.file
         /// <returns>一時コピーしたファイルのパス</returns>
         public static string CreateTempCopy( string a_filePath )
         {
-            string dir = System.IO.Path.GetDirectoryName( a_filePath );
+            /* 元ファイルと同じフォルダではなく、OSのTempフォルダに作成する
+             * （元フォルダが読み取り専用の場合でもコピーできるようにするため）
+             */
+            string dir = System.IO.Path.GetTempPath();
             string tempPath = System.IO.Path.Combine( dir, System.IO.Path.GetFileNameWithoutExtension( a_filePath ) + "_" + DateTime.Now.ToString( "yyyyMMdd_HHmmss" ) + System.IO.Path.GetExtension( a_filePath ) );
             System.IO.File.Copy( a_filePath, tempPath, false );
             return tempPath;
